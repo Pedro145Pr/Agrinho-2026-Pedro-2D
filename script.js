3e87cdca64f53b5f67e916bdeeb2869e6ffa641f
Pedro145Pr <lopes.ferreira01@escola.pr.gov.br>
2026-06-17 13:58:41 -0300

    Melhoria

--- script.js
+++ script.js
@@ -2,8 +2,3 @@
 function ir(pagina) {
     window.location.href = pagina;
 }
-
-function mensagem() {
-    document.getElementById("msg").innerHTML =
-        "🌱 O Agrinho mostra que a educação e o campo juntos podem construir um futuro melhor e mais sustentável!";
-}
