b0705ec5bf3ed7f008838a8f205e51ae05ec4805
Pedro145Pr <lopes.ferreira01@escola.pr.gov.br>
2026-06-17 13:34:41 -0300

    Site do chat

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ avanca.forEach(button => {
         document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
+
+function mensagem() {
+    document.getElementById("msg").innerHTML =
+        "🌱 O Agrinho mostra que a educação e o campo juntos podem construir um futuro melhor e mais sustentável!";
+}
